b9d75d5a172f486691c29414ec2e70f1438a663d
marc <marc@mkdr-ZEN>
2014-04-23 02:00:12 +0200

    v1.8, first step into new event system

--- src/gui/GW2EventerGui.java
+++ src/gui/GW2EventerGui.java
@@ -227,7 +227,7 @@ public class GW2EventerGui extends javax.swing.JFrame {
     
     public static final int EVENT_COUNT = 23;
     
-    private static final String VERSION = "1.77";
+    private static final String VERSION = "1.8";
     
     private JButton workingButton;
     private JCheckBox refreshSelector;
